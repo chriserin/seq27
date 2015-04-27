a34e40097d600d2f3c54d3fce45d72d2da98e7a3
Christopher Erin <chris.erin@gmail.com>
2015-04-26 19:50:58 -0500

    Add settings for capachrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,9 +5,27 @@ ActionController::Base.allow_rescue = false
 FactoryGirl.definition_file_paths = %w(spec/factories)
 FactoryGirl.find_definitions
 
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
 DatabaseCleaner.strategy = :transaction
 Cucumber::Rails::Database.javascript_strategy = :transaction
 
 World(FactoryGirl::Syntax::Methods)
+
+if true #quick and dirty switching between poltergeist and capachrome
+  require 'capybara/poltergeist'
+  Capybara.javascript_driver = :poltergeist
+else
+  Capybara.register_driver :capachrome do |app|
+    Capybara::Capachrome::Driver.new(app, browser: :chrome, args: ["enable-web-midi"])
+  end
+  Capybara.javascript_driver = :capachrome
+
+  module ChromeLogDisplay
+    def display_logs
+      page.driver.getLog(:browser).reject {|x| x =~ /Download the React DevTools/}.each do |message|
+        puts message
+      end
+    end
+  end
+
+  World(ChromeLogDisplay)
+end
