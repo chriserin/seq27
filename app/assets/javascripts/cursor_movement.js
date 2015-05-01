3b0152d4448800d71fa1e8b8597069e29d8527c1
Chris Erin and Josh Branchaud <dev+chriserin+jbranchaud@hashrocke.com>
2015-05-01 16:12:52 -0500

    Javascript inc and dec ops ffor movement

--- app/assets/javascripts/cursor_movement.js
+++ app/assets/javascripts/cursor_movement.js
@@ -1,21 +1,22 @@
 window.CursorMovement = {}
 
 CursorMovement.moveDown = function(state) {
-  state['cursor']['pitch'] = state['cursor']['pitch'] + 1
+  //state['cursor']['pitch'] = state['cursor']['pitch'] + 1
+  state['cursor']['pitch']++;
   return state;
 }
 
 CursorMovement.moveUp = function(state) {
-  state['cursor']['pitch'] = state['cursor']['pitch'] - 1
+  state['cursor']['pitch']--;
   return state;
 }
 
 CursorMovement.moveRight = function(state) {
-  state['cursor']['beats'] = state['cursor']['beats'] + 1
+  state['cursor']['beats']++;
   return state;
 }
 
 CursorMovement.moveLeft = function(state) {
-  state['cursor']['beats'] = state['cursor']['beats'] - 1
+  state['cursor']['beats']--;
   return state;
 }
