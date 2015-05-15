6f191c19825a6393b5f9a7b6d54fbf8ea9349a41
Chris Erin <chris.erin+chriserin@gmail.com>
2015-05-14 20:17:24 -0500

    Play song feature runs with capachrome

---
+++ features/artist_plays_a_song.feature
@@ -0,0 +1,8 @@
+Feature: Artist plays a song
+
+  @javascript
+  Scenario: Artist plays a song
+    Given a signed in artist with a song
+    When  I am on the song page
+    And   I press the space bar
+    Then  I hear the song (via midi)
--- features/support/env.rb
+++ features/support/env.rb
@@ -10,22 +10,23 @@ Cucumber::Rails::Database.javascript_strategy = :transaction
 
 World(FactoryGirl::Syntax::Methods)
 
-if true #quick and dirty switching between poltergeist and capachrome
+Capybara.register_driver :capachrome do |app|
+  Capybara::Capachrome::Driver.new(app, browser: :chrome, args: ["enable-web-midi"])
+end
+
+module ChromeLogDisplay
+  def display_logs
+    page.driver.getLog(:browser).reject {|x| x =~ /Download the React DevTools/}.each do |message|
+      puts message
+    end
+  end
+end
+
+World(ChromeLogDisplay)
+
+if false #quick and dirty switching between poltergeist and capachrome
   require 'capybara/poltergeist'
   Capybara.javascript_driver = :poltergeist
 else
-  Capybara.register_driver :capachrome do |app|
-    Capybara::Capachrome::Driver.new(app, browser: :chrome, args: ["enable-web-midi"])
-  end
   Capybara.javascript_driver = :capachrome
-
-  module ChromeLogDisplay
-    def display_logs
-      page.driver.getLog(:browser).reject {|x| x =~ /Download the React DevTools/}.each do |message|
-        puts message
-      end
-    end
-  end
-
-  World(ChromeLogDisplay)
 end
