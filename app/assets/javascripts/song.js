0964bf004c224b9db58bf16d1053fa9fac4afb98
Christopher Erin <chris.erin@gmail.com>
2015-05-17 11:50:22 -0500

    Stub out song play

--- app/assets/javascripts/song.js
+++ app/assets/javascripts/song.js
@@ -6,6 +6,9 @@ Song.addNote = function(songState) {
 }
 
 Song.play = function(songState) {
+  Midi.sendOn(1, 80, 80);
+  Midi.sendOn(1, 80, 80);
+  // setTimeout(function() {Midi.sendOff(1, 80, 80)}, 200);
 
   return songState;
 }
