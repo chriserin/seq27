025c11be21bb264223beed480741cc6e5cef63a4
Christopher Erin <chris.erin@gmail.com>
2015-05-19 19:22:30 -0500

    Test that a midi note has duration

--- app/assets/javascripts/song.js
+++ app/assets/javascripts/song.js
@@ -6,9 +6,10 @@ Song.addNote = function(songState) {
 }
 
 Song.play = function(songState) {
+  SCHEDULE_DELAY_TIME = 250;
   for(var note of songState.song.notes) {
-    Midi.sendOn(1, note.pitch, 80);
-    Midi.sendOff(1, note.pitch, 80);
+    Midi.sendOn(1, note.pitch, 80, 0 + SCHEDULE_DELAY_TIME);
+    Midi.sendOff(1, note.pitch, 80, 10 + SCHEDULE_DELAY_TIME);
   }
 
   return songState;
