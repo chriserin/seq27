49681714e75f103f29d9d5d18b2573b26c333198
Christopher Erin <chris.erin@gmail.com>
2015-05-23 09:42:22 -0500

    Calculate note length based on tempo

--- app/assets/javascripts/song.js
+++ app/assets/javascripts/song.js
@@ -8,8 +8,12 @@ Song.addNote = function(songState) {
 Song.play = function(songState) {
   SCHEDULE_DELAY_TIME = 250;
   for(var note of songState.song.notes) {
+    bpm = 6000
+    seconds_per_tick = 60 / (96.0 * bpm)
+
+    note_length = note.ticks * (seconds_per_tick * 1000)
     Midi.sendOn(1, note.pitch, 80, 0 + SCHEDULE_DELAY_TIME);
-    Midi.sendOff(1, note.pitch, 80, 10 + SCHEDULE_DELAY_TIME);
+    Midi.sendOff(1, note.pitch, 80, note_length + SCHEDULE_DELAY_TIME);
   }
 
   return songState;
