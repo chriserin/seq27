d5cd4b488718a3347d8203b0d9fadd1c5798b401
Christopher Erin <chris.erin@gmail.com>
2015-05-25 10:40:31 -0500

    Each message on its own line

--- spec/chrome_log_display.rb
+++ spec/chrome_log_display.rb
@@ -1,6 +1,7 @@
 module ChromeLogDisplay
   def display_logs
     browser_messages = page.driver.getLog(:browser)
-    puts browser_messages.reject {|x| x =~ /Download the React DevTools/}
+    filtered_messages = browser_messages.reject {|x| x =~ /Download the React DevTools/}
+    filtered_messages.each { |m| puts m; }
   end
 end
