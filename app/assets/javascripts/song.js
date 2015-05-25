0eafbd64972c2734055329db15f6266be126a833
Christopher Erin <chris.erin@gmail.com>
2015-05-25 10:43:01 -0500

    Determine seconds per tick once

--- app/assets/javascripts/song.js
+++ app/assets/javascripts/song.js
@@ -7,13 +7,13 @@ Song.addNote = function(songState) {
 
 Song.play = function(songState) {
   SCHEDULE_DELAY_TIME = 1000;
-  for(var note of songState.song.notes) {
-    bpm = songState.song.tempo;
-    seconds_per_tick = 60 / (96.0 * bpm);
+  bpm = songState.song.tempo;
+  seconds_per_tick = 60 / (96.0 * bpm);
 
-    note_length = note.ticks * (seconds_per_tick * 1000)
+  for(var note of songState.song.notes) {
+    note_length_in_millis = note.ticks * (seconds_per_tick * 1000)
     Midi.sendOn(1, note.pitch, 80, 0 + SCHEDULE_DELAY_TIME);
-    offTime = note_length + SCHEDULE_DELAY_TIME;
+    offTime = note_length_in_millis + SCHEDULE_DELAY_TIME;
     Midi.sendOff(1, note.pitch, 80, offTime);
   }
 
