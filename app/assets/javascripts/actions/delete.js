947ed5b123a79b9584bb505835dcc6cdedf92c7d
Chris Erin <chris.erin+chriserin@gmail.com>
2015-10-09 13:53:57 -0500

    Compact notes after deleting

--- app/assets/javascripts/actions/delete.js
+++ app/assets/javascripts/actions/delete.js
@@ -3,10 +3,13 @@ window.Delete = {}
 Delete.deleteLatest = function(songState) {
   var notes = SongState.currentGroupNotes(songState)
   var part = SongState.activePart()
+
   for (var note of notes) {
     var index = SongState.indexOfNote(note, part)
     delete part.notes[index];
   }
+
+  part.notes = part.notes.filter(function(n) { return n;})
   return songState;
 }
 
