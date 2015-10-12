489d4fbef9ea008455612e0ca2b79587ced1d1c5
Chris Erin <chris.erin+chriserin@gmail.com>
2015-10-12 08:22:49 -0500

    Apply styles for visual selection

--- app/assets/javascripts/components/visual_selection.js.jsx
+++ app/assets/javascripts/components/visual_selection.js.jsx
@@ -1,13 +1,26 @@
 window.SeqCom = window.SeqCom || {}
 
 SeqCom.VisualSelection = React.createClass({
+  styles() {
+    return {
+      top: 16 * (127 - this.props.top),
+      left: 60 + (75 * (this.props.left / 96)),
+      height: 16 * (this.props.top - this.props.bottom + 1),
+      width: (75 * (this.props.right / 96 - this.props.left / 96))
+    }
+  },
   render: function() {
-    return <visualSelection
-              data-left={this.props.left}
-              data-right={this.props.right}
-              data-top={this.props.top}
-              data-bottom={this.props.bottom}
-           >
-           </visualSelection>;
+    if (ViewState.mode === 'visual') {
+      return <visualSelection
+                data-left={this.props.left}
+                data-right={this.props.right}
+                data-top={this.props.top}
+                data-bottom={this.props.bottom}
+                style={this.styles()}
+             >
+             </visualSelection>;
+    }
+    else
+      return <span/>
   }
 })
