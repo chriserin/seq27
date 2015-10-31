015c9b01f1cabf0b4f37179f05ebfa0a33bccbb2
Chris Erin and Mike Chau <chris.erin+chriserin+mikechau@gmail.com>
2015-10-31 12:26:51 -0500

    Read application yml before configuration

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Seq27
     config.action_dispatch.perform_deep_munge = false
     config.active_record.schema_format = :sql
 
-    config.before_initialize do
+    config.before_configuration do
       dev = Rails.root.join 'config/application.yml'
       YAML.load_file(dev.to_s).each do |key, value|
         ENV[key.to_s] = value.to_s unless ENV[key.to_s]
