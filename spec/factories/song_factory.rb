5f0583136a73c14251708e5679ad216da891d663
Chris Erin <chris.erin+chriserin@gmail.com>
2016-06-05 18:08:57 -0500

    Remove name column from song factory

--- spec/factories/song_factory.rb
+++ spec/factories/song_factory.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :song do
-    name "SongA"
     definition <<-SONG
       { "tempo": 60, "arrangement": [0], "sections": [{"loop": 1, "parts": [{"beats": 4, "notes": []}]}]}
     SONG
